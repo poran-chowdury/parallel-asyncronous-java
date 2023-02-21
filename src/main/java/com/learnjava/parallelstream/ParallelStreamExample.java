87048c72064c3114808df862508f09808da631a6
poran chowdury <poran.chowdury@selise.ch>
2023-02-22 01:05:45 +0600

    refactor with parallel stream

---
+++ src/main/java/com/learnjava/parallelstream/ParallelStreamExample.java
@@ -0,0 +1,32 @@
+package com.learnjava.parallelstream;
+
+import com.learnjava.util.DataSet;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.*;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ParallelStreamExample {
+
+    public List<String> stringTreasFormation(List<String> nameList) {
+        return nameList.parallelStream()
+                .map(this::addNameLengthTransform)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<String> namesList = DataSet.namesList();
+        ParallelStreamExample streamExample = new ParallelStreamExample();
+        startTimer();
+        List<String> resultList = streamExample.stringTreasFormation(namesList);
+        log("result is : "+resultList);
+        timeTaken();
+    }
+
+    private  String addNameLengthTransform(String name) {
+        delay(500);
+        return name.length() + " - " + name;
+    }
+}
