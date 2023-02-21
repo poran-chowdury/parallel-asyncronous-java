11944475acaea7512edf8494ba60b42e8f169ef8
poran chowdury <poran.chowdury@selise.ch>
2023-02-21 20:57:24 +0600

    add thread to improve the total time

--- src/main/java/com/learnjava/service/ProductService.java
+++ src/main/java/com/learnjava/service/ProductService.java
@@ -3,6 +3,7 @@ package com.learnjava.service;
 import com.learnjava.domain.Product;
 import com.learnjava.domain.ProductInfo;
 import com.learnjava.domain.Review;
+
 import static com.learnjava.util.CommonUtil.stopWatch;
 import static com.learnjava.util.LoggerUtil.log;
 
@@ -15,18 +16,26 @@ public class ProductService {
         this.reviewService = reviewService;
     }
 
-    public Product retrieveProductDetails(String productId) {
+    public Product retrieveProductDetails(String productId) throws InterruptedException {
         stopWatch.start();
-
-        ProductInfo productInfo = productInfoService.retrieveProductInfo(productId); // blocking call
-        Review review = reviewService.retrieveReviews(productId); // blocking call
-
+        ProductInfoRunnable productInfoRunable = new ProductInfoRunnable(productId);
+        Thread productThread = new Thread(productInfoRunable);
+        ReviewInfoRunable reviewInfoRunable = new ReviewInfoRunable(productId);
+        Thread reviewThread = new Thread(reviewInfoRunable);
+//        ProductInfo productInfo = productInfoService.retrieveProductInfo(productId); // blocking call
+//        Review review = reviewService.retrieveReviews(productId); // blocking call
+        productThread.start();
+        reviewThread.start();
+        productThread.join();
+        reviewThread.join();
+        ProductInfo productInfo = productInfoRunable.getProductInfo();
+        Review review = reviewInfoRunable.getReview();
         stopWatch.stop();
-        log("Total Time Taken : "+ stopWatch.getTime());
+        log("Total Time Taken : " + stopWatch.getTime());
         return new Product(productId, productInfo, review);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ProductInfoService productInfoService = new ProductInfoService();
         ReviewService reviewService = new ReviewService();
@@ -34,6 +43,43 @@ public class ProductService {
         String productId = "ABC123";
         Product product = productService.retrieveProductDetails(productId);
         log("Product is " + product);
+
+    }
+
+    public class ReviewInfoRunable implements Runnable {
+        private String productId;
+        private Review review;
+
+        public ReviewInfoRunable(String productId) {
+            this.productId = productId;
+        }
+
+        public Review getReview() {
+            return review;
+        }
+
+        @Override
+        public void run() {
+            review = reviewService.retrieveReviews(this.productId);
+        }
+    }
+
+    public class ProductInfoRunnable implements Runnable {
+        private ProductInfo productInfo;
+        private String productId;
+
+        public ProductInfoRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            productInfo = productInfoService.retrieveProductInfo(productId);
+        }
+
+        public ProductInfo getProductInfo() {
+            return productInfo;
+        }
 
     }
 }
