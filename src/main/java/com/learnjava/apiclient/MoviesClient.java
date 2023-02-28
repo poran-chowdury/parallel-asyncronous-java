eb3706c1c3ebcd0286ee40a1e885a31c0e22980d
poran chowdury <poran.chowdury@selise.ch>
2023-02-28 12:53:21 +0600

    refactor with completable future

--- src/main/java/com/learnjava/apiclient/MoviesClient.java
+++ src/main/java/com/learnjava/apiclient/MoviesClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class MoviesClient {
     private final WebClient webClient;
@@ -18,7 +19,15 @@ public class MoviesClient {
     public Movie retrieveMovie(Long movieInfoId) {
         MovieInfo movieInfo = invokeMovieInfoServices(movieInfoId);
         List<Review> reviews = invokeReviewServices(movieInfoId);
-        return new Movie(movieInfo,reviews);
+        return new Movie(movieInfo, reviews);
+    }
+
+    public CompletableFuture<Movie> retrieveMovie_CF(Long movieInfoId) {
+        var movieInfoCompletableFuture = CompletableFuture.supplyAsync(() -> invokeMovieInfoServices(movieInfoId));
+        var reviews = CompletableFuture.supplyAsync(() -> invokeReviewServices(movieInfoId));
+
+        return movieInfoCompletableFuture
+                .thenCombine(reviews, Movie::new);
     }
 
     private MovieInfo invokeMovieInfoServices(Long movieInfoId) {
@@ -26,11 +35,12 @@ public class MoviesClient {
         var moviesInfoUrlPath = "/v1/movie_infos/{movieInfoId}";
         return webClient
                 .get()
-                .uri(moviesInfoUrlPath,movieInfoId)
+                .uri(moviesInfoUrlPath, movieInfoId)
                 .retrieve()
                 .bodyToMono(MovieInfo.class)
                 .block();
     }
+
     private List<Review> invokeReviewServices(Long movieInfoId) {
 
         var reviewBasePath = "/v1/reviews";
--- src/test/java/com/learnjava/apiclient/MoviesClientTest.java
+++ src/test/java/com/learnjava/apiclient/MoviesClientTest.java
@@ -1,9 +1,13 @@
 package com.learnjava.apiclient;
 
 import com.learnjava.domain.movie.Movie;
+import com.learnjava.util.CommonUtil;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoviesClientTest {
@@ -13,7 +17,7 @@ class MoviesClientTest {
 
     private MoviesClient moviesClient = new MoviesClient(webClient);
 
-    @Test
+    @RepeatedTest(10)
     void retrieveMovie() {
         /*
          * {
@@ -29,10 +33,39 @@ class MoviesClientTest {
          */
         // given
         var movieInfoId = 1L;
-
+        startTimer();
         // when
         var movie = moviesClient.retrieveMovie(movieInfoId);
         System.out.println("movie: "+ movie);
+        CommonUtil.timeTaken();
+        // then
+        assert movie != null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assert movie.getReviewList().size() == 1;
+
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovie_CF() {
+        /*
+         * {
+         *   "movieInfoId": 1,
+         *   "name": "Batman Begins",
+         *   "year": 2005,
+         *   "cast": [
+         *     "Christian Bale",
+         *     "Michael Cane"
+         *   ],
+         *   "release_date": "2005-06-15"
+         * }
+         */
+        // given
+        var movieInfoId = 1L;
+        startTimer();
+        // when
+        var movie = moviesClient.retrieveMovie_CF(movieInfoId).join();
+        System.out.println("movie: "+ movie);
+        timeTaken();
         // then
         assert movie != null;
         assertEquals("Batman Begins", movie.getMovieInfo().getName());
